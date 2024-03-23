ebf7c367afc10582e3f4fc77d7dfb4b542dd25b6
Lawson-JR <lawson5n2010@gmail.com>
2024-03-23 12:01:03 -0700

    Using puppet for configurations

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Using puppet to change our configuration file
+
+file { 'etc/ssh/ssh_config':
+	ensure => present,
+
+content =>"
+
+	#SSH client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentification no
+}
